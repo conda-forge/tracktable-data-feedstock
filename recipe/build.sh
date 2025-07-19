7de279a112df4ba0fbec95fc0465ad74749748c8
Andy Wilson <atwilso@sandia.gov>
2025-07-19 15:25:40 -0600

    Run find in appropriate directory
    Find needs to run in $SRC_DIR/tracktable-data and ignore the
    .git subdirectory.

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,10 +20,13 @@ echo "--- Running du"
 du $SRC_DIR
 
 echo "--- Checking for smudge files"
-find . -type f | xargs grep -l "version https://git-lfs.github.com/spec/v1"
+find \$SRC_DIR/tracktable-data -type f \
+   | grep -v .git \
+   | xargs grep -l "version https://git-lfs.github.com/spec/v1"
 status=$?
 if [ $status -eq 0 ]; then
   echo "ERROR: Smudge files found in repository"
+  exit 1
 fi
 
 
